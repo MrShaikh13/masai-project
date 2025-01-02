ab3c49d20548107b2fa4f2ed944c0531a119d7ea
MrShaikh13 <161179713+MrShaikh13@users.noreply.github.com>
2025-01-02 22:37:08 +0530

    Update index3.js

--- index3.js
+++ index3.js
@@ -1,14 +1,22 @@
-let arr =  [[5,4,3,2,1],
-            [6,7,8,9,1],
-            [3,2,5,4,6],
-            [7,8,9,1,2]
-        ];
-let N = 4;
-let M = 5;
+let arr = [
+  [1, 2, 3, 4, 5],
+  [6, 7, 8, 9, 1],
+  [3, 2, 5, 4, 6],
+  [7, 8, 9, 1, 2],
+];
+
 let str = "";
-for(i=0; i<N; i++){
-    for(j=0; j<M; j++){
-        str += arr[i][j] + " "
+
+for (let i = 0; i < arr.length; i++) {
+  if (i % 2 != 0) {
+    for (let j = 0; j < arr[i].length; j++) {
+      str += arr[i][j] + " ";
     }
+  } else {
+    for (let k = arr[i].length - 1; k >= 0; k--) {
+      str += arr[i][k] + " ";
+    }
+  }
 }
-console.log(str)
+
+console.log(str);
